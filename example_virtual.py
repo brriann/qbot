ff5b8244c63c5e756f3fff8889d6005e000a4dae
Brian Foster <brriann@gmail.com>
2019-02-04 22:42:08 -0700

    add comments

--- example_virtual.py
+++ example_virtual.py
@@ -21,14 +21,24 @@ r = Renderer(100)
 q = np.random.rand(e.bot.observation_space(), e.bot.action_space())
 
 # try changing these hyper-parameters...
-explore = 0.1   # exploration rate (odds of taking a random action)
-alpha = 0.01    # learning rate (proportional weight of new v. old information)
-gamma = 0.6     # discount rate (relative value of future v. current reward)
+
+# exploration rate (odds of taking a random action)
+# 0.0 - 1.0
+explore = 0.1   
+
+# learning rate (proportional weight of new v. old information)
+# 0.00 - 1.00
+alpha = 0.01    
+
+# discount rate (relative value of future v. current reward)
+# 0.0 - 1.0                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                         
+gamma = 0.6     
 
 try:
+    # default is range(1000000)
     for n in range(1000000):
         state = e.reset(obstacle_count=1)
-        r.render_reset(e, raytrace=True)
+        r.render_reset(e, raytrace=False)
         done = False
         steps = 0
         while not done:
